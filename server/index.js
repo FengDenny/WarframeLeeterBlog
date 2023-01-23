42c49df856500edb49a6cec852950ca6ec9698e0
FengDenny <fengdennyy@gmail.com>
2023-01-23 15:42:17 -0800

    Refactored routes to own files

--- server/index.js
+++ server/index.js
@@ -3,22 +3,17 @@ const cors = require("cors");
 const port = process.env.PORT || 5000;
 const app = express();
 
-const request = require("request");
-
-const { GET_ALL_BADGES } = require("./Models/queries/badges/badges");
+const { readdirSync } = require("fs");
 
 app.use(cors());
+
 // Body Parser START
 app.use(express.json({ limit: "10kb" }));
 app.use(express.urlencoded({ extended: true, limit: "10kb" }));
 // Body Parser END
 
-app.get("/getBadges", (res, req) => {
-  request(
-    `https://leetcode.com/graphql?query=${GET_ALL_BADGES}`,
-    function (error, response, body) {
-      req.send(body);
-    }
-  );
-});
+readdirSync("./routes").map((route) =>
+  app.use("/", require(`./routes/${route}`))
+);
+
 app.listen(port, console.log(`Server is running on port ${port}`));
