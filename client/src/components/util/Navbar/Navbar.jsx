9e339105656e7da381ef4147c135dec604492f70
FengDenny <fengdennyy@gmail.com>
2023-01-27 10:32:49 -0800

    Navbar desktop/mobile created

---
+++ client/src/components/util/Navbar/Navbar.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import navbarStyles from "../../../scss/Navbar/navbar.module.scss";
+import globalStyles from "../../../scss/global/global.module.scss";
+import DesktopNavbar from "./Desktop/DesktopNavbar";
+import MobileNavbar from "./Mobile/MobileNavbar";
+import { MobileMenuToggler } from "../../../View/utils/Navbar/NavbarView";
+export default function Navbar() {
+  const { mobileMenuOpen, toggleOpen, closeMobileMenu } = MobileMenuToggler();
+  return (
+    <div className={navbarStyles.container}>
+      <nav className={navbarStyles.nav}>
+        <DesktopNavbar />
+        <MobileNavbar
+          show={mobileMenuOpen}
+          toggle={toggleOpen}
+          close={closeMobileMenu}
+        />
+      </nav>
+    </div>
+  );
+}
