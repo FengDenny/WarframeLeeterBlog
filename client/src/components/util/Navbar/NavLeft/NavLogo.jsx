5355151b339d5216c80661d823d2100a011bc13d
FengDenny <fengdennyy@gmail.com>
2023-01-27 14:14:16 -0800

    Updated logo to switch logo based on theme toggle

--- client/src/components/util/Navbar/NavLeft/NavLogo.jsx
+++ client/src/components/util/Navbar/NavLeft/NavLogo.jsx
@@ -1,10 +1,20 @@
 import navbarStyles from "../../../../scss/Navbar/navbar.module.scss";
-
+import { useSelector } from "react-redux";
 export default function NavLogo() {
-  return (
+  const { themeData } = useSelector((state) => ({ ...state }));
+
+  const { toggled } = themeData;
+
+  return !toggled ? (
     <img
       src={require("../../../../images/badge_logo/white_badge.png")}
       alt='white-logo'
+      className={navbarStyles.logo}
+    />
+  ) : (
+    <img
+      src={require("../../../../images/badge_logo/colored_badge.png")}
+      alt='colored-logo'
       className={navbarStyles.logo}
     />
   );
