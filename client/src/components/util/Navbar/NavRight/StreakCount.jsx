93790617251b7d857a661de491ab984b83b13099
FengDenny <fengdennyy@gmail.com>
2023-01-27 14:15:48 -0800

    Retrieve streak data on Public Profile component

--- client/src/components/util/Navbar/NavRight/StreakCount.jsx
+++ client/src/components/util/Navbar/NavRight/StreakCount.jsx
@@ -1,11 +1,19 @@
 import React from "react";
 import navbarStyles from "../../../../scss/Navbar/navbar.module.scss";
 import { FaFire } from "react-icons/fa";
+import { PublicProfile } from "../../../../View/utils/Navbar/profile/PublicProfile";
 
 export default function StreakCount() {
+  const { data } = PublicProfile();
+
   return (
-    <div className={navbarStyles.streakIcon}>
-      <FaFire />
-    </div>
+    <>
+      <div className={navbarStyles.streakIcon}>
+        <FaFire />
+        <span className={navbarStyles.streakData}>
+          {data && data.matchedUser.userCalendar.streak}
+        </span>
+      </div>
+    </>
   );
 }
