bdc3e6bbcdc7bc8291f07db444b2ab01bc4887d9
FengDenny <fengdennyy@gmail.com>
2023-01-27 10:34:34 -0800

    Created Searchbar component

---
+++ client/src/components/util/Navbar/NavRight/Searchbar.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import navbarStyles from "../../../../scss/Navbar/navbar.module.scss";
+import mobileStyles from "../../../../scss/Navbar/mobileNavbar.module.scss";
+import { FaSearch } from "react-icons/fa";
+export default function Searchbar({ mobile }) {
+  return (
+    <form
+      className={`${
+        mobile
+          ? mobileStyles.searchbarContainer
+          : navbarStyles.searchbarContainer
+      }`}
+      id='form-search'
+    >
+      <input
+        type='search'
+        name='search'
+        placeholder='Two Sum'
+        aria-label='Search solutions by title'
+        results='5'
+        className={`${
+          mobile ? mobileStyles.searchbar : navbarStyles.searchbar
+        }`}
+      />
+      <div
+        className={`${
+          mobile ? mobileStyles.btnContainer : navbarStyles.btnContainer
+        }`}
+      >
+        <button
+          className={`${
+            mobile ? mobileStyles.searchBtn : navbarStyles.searchBtn
+          }`}
+        >
+          <FaSearch
+            className={`${
+              mobile ? mobileStyles.faSearch : navbarStyles.faSearch
+            }`}
+          />
+        </button>
+      </div>
+    </form>
+  );
+}
