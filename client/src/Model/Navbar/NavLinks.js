3819115ae7902a5d806f61119a0781f823a8d2ea
FengDenny <fengdennyy@gmail.com>
2023-01-27 10:36:31 -0800

    Nav links json data

---
+++ client/src/Model/Navbar/NavLinks.js
@@ -0,0 +1,17 @@
+export const links = [
+  {
+    id: 1,
+    name: "Home",
+    ahref: "/",
+  },
+  {
+    id: 2,
+    name: "Achievements",
+    ahref: "/achievements",
+  },
+  {
+    id: 3,
+    name: "Solutions",
+    ahref: "/solutions",
+  },
+];
