22a66e81c2f3428486077dafee2bd1d4b81035e7
FengDenny <fengdennyy@gmail.com>
2023-01-30 10:47:27 -0800

    Updated to a pure Card component

--- client/src/components/util/Card/Card.jsx
+++ client/src/components/util/Card/Card.jsx
@@ -1,20 +1,5 @@
 import React from "react";
-import Banner from "../Banner/Banner";
-
-import { useRemoveDuplicatedSubmissions } from "../../../View/queries/dailyChallenge/hooks/useRemoveDuplicates";
-import { useGetRecentSubmission } from "../../../View/queries/dailyChallenge/hooks/useGetRecentSubmission";
 
 export default function Card({ styling, children }) {
-  // console.log(submissions);
-  const { data } = useGetRecentSubmission("warframeleeter");
-
-  const { submissions } = useRemoveDuplicatedSubmissions(data);
-
-  console.log(submissions);
-  return (
-    <div className={styling}>
-      <Banner title={`Completed`} />
-      {children}
-    </div>
-  );
+  return <div className={styling}>{children}</div>;
 }
