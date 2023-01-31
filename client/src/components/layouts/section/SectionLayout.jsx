5a3f6b18c20f6fd8dbd28d9cd3563ea7808de044
FengDenny <fengdennyy@gmail.com>
2023-01-31 12:47:11 -0800

    Added layout for innerHTML props

--- client/src/components/layouts/section/SectionLayout.jsx
+++ client/src/components/layouts/section/SectionLayout.jsx
@@ -2,13 +2,29 @@ import React from "react";
 import layoutStyles from "../../../scss/Section/SectionLayout.module.scss";
 import globalStyle from "../../../scss/global/global.module.scss";
 
-export default function SectionLayout({ style, title, description, children }) {
+export default function SectionLayout({
+  style,
+  title,
+  description,
+  innerHtmlDescription,
+  button,
+  children,
+}) {
   return (
     <section className={style}>
       <div className={globalStyle.container}>
         <div className={`${layoutStyles.header}`}>
           <h2 className={layoutStyles.heading}>{title}</h2>
-          <h4 className={layoutStyles.description}>{description}</h4>
+          <h4
+            className={`${
+              innerHtmlDescription
+                ? layoutStyles.innerHtml
+                : layoutStyles.description
+            }`}
+          >
+            {description}
+          </h4>
+          {button && button}
         </div>
       </div>
       <div className={globalStyle.container}>{children}</div>
