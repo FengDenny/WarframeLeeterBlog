66b27813ae1c55b50b9619639c8621c8f7d7052b
FengDenny <fengdennyy@gmail.com>
2023-01-31 12:45:52 -0800

    Added Solution section

--- client/src/Pages/Home/Home.jsx
+++ client/src/Pages/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from "react";
 import Hero from "./Hero";
+import Solution from "./Solution";
 
 export default function Home() {
   return (
     <div>
       <Hero />
+      <Solution />
     </div>
   );
 }
