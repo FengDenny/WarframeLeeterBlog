c330fea676177c1e73723bbe3076bd759d47de19
FengDenny <fengdennyy@gmail.com>
2023-01-31 12:46:55 -0800

    Added styling prop instead of hard coded styling

--- client/src/components/util/Banner/Banner.jsx
+++ client/src/components/util/Banner/Banner.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import bannerStyle from "../../../scss/util/Banner/Banner.module.scss";
 
-export default function Banner({ title }) {
+export default function Banner({ title, style }) {
   return (
-    <div className={`${bannerStyle.container} ${bannerStyle.green}`}>
+    <div className={`${bannerStyle.container} ${style}`}>
       <h2 className={bannerStyle.title}>{title}</h2>
     </div>
   );
