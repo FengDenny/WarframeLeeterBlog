3106c9bcb38cb8cd8acad7751dcb90c068612f82
FengDenny <fengdennyy@gmail.com>
2023-01-31 12:45:40 -0800

    Solution section created

---
+++ client/src/Pages/Home/Solution.jsx
@@ -0,0 +1,82 @@
+import React, { Fragment } from "react";
+import SolutionLayout from "../../components/layouts/section/SectionLayout";
+import layoutStyles from "../../scss/Section/SectionLayout.module.scss";
+
+import Card from "../../components/util/Card/Card";
+import cardStyles from "../../scss/util/Card/Card.module.scss";
+
+import { useNavigate } from "react-router-dom";
+import { threeRecentSolutionData } from "../../View/queries/solution/SolutionTopic";
+import { lcWebURL } from "../../Controller/endpoint";
+
+export default function Solution() {
+  const navigate = useNavigate();
+
+  const { solutionsData } = threeRecentSolutionData();
+
+  const recentCardSolution = () => {
+    const solution =
+      solutionsData &&
+      solutionsData.map((item) => {
+        const { title, url, viewCount, questionTitle, id, voteCount } = item;
+
+        return (
+          <Card styling={cardStyles.solutionCard} key={id}>
+            <div className={cardStyles.solutionCardContainer}>
+              <div
+                className={`${cardStyles.header} ${cardStyles.solutionHeader}`}
+              >
+                <h2 className={cardStyles.heading}>{questionTitle}</h2>
+              </div>
+              <ul className={cardStyles.lists}>
+                <li>
+                  <a href={lcWebURL(url)} target='_blank' rel='noreferrer'>
+                    {title}
+                  </a>
+                </li>
+              </ul>
+
+              <div className={cardStyles.solutionCount}>
+                <h4 className={cardStyles.voteHeading}>
+                  <span>{voteCount} </span> vote count
+                </h4>
+                <h4 className={cardStyles.viewHeading}>
+                  <span>{viewCount} </span> views
+                </h4>
+              </div>
+            </div>
+          </Card>
+        );
+      });
+    return solution;
+  };
+
+  return (
+    <SolutionLayout
+      style={layoutStyles.solutionSection}
+      layout
+      title='LeetCode Solutions'
+      innerHtmlDescription
+      description={
+        <Fragment>
+          For all problems solved, each solution had been explained thoroughly,
+          and solved with{" "}
+          <span className={layoutStyles.spanDescription}> Python</span> and
+          often times with whiteboard walkthrough’s .
+        </Fragment>
+      }
+      button={
+        <Fragment>
+          <button
+            className={layoutStyles.button}
+            onClick={() => navigate("/solutions")}
+          >
+            Search Solution
+          </button>
+        </Fragment>
+      }
+    >
+      {recentCardSolution()}
+    </SolutionLayout>
+  );
+}
