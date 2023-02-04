422aab75b36e29bd3a4cd068112bb16205fc2c4d
FengDenny <fengdennyy@gmail.com>
2023-02-03 16:15:39 -0800

    Added DailysSolved Section

--- client/src/Pages/Home/Home.jsx
+++ client/src/Pages/Home/Home.jsx
@@ -1,12 +1,14 @@
 import React, { useState, useEffect } from "react";
 import Hero from "./Hero";
 import Solution from "./Solution";
+import DailysSolved from "./DailysSolved";
 
 export default function Home() {
   return (
     <div>
       <Hero />
       <Solution />
+      <DailysSolved />
     </div>
   );
 }
