ee3f2f0d4f5b8c93ce69e56c150b25288e704045
FengDenny <fengdennyy@gmail.com>
2023-02-09 15:05:13 -0800

    Added / completed  Statistics section

--- client/src/Pages/Home/Home.jsx
+++ client/src/Pages/Home/Home.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import Hero from "./Hero";
 import Solution from "./Solution";
 import DailysSolved from "./DailysSolved";
+import Statistics from "./Statistics";
 
 export default function Home() {
   return (
@@ -9,6 +10,7 @@ export default function Home() {
       <Hero />
       <Solution />
       <DailysSolved />
+      <Statistics />
     </div>
   );
 }
