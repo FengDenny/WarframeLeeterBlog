bfa3bbb5a0c3bdf994aa99af5dc14520a4936da8
FengDenny <fengdennyy@gmail.com>
2023-02-09 15:04:50 -0800

    Created Statistic section to display StatsView layout

---
+++ client/src/Pages/Home/Statistics.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import SolutionLayout from "../../components/layouts/section/SectionLayout";
+import statsStyles from "../../scss/Section/SectionLayout.module.scss";
+
+import {
+  AllProblemsSolved,
+  languageStats,
+  topicStats,
+} from "../../View/queries/statistics/StatsView";
+
+export default function Statistics() {
+  return (
+    <SolutionLayout
+      style={statsStyles.statsSection}
+      title={" WarframeLeeter Statistics"}
+      description={
+        "My real time statistic data based on past and future problems"
+      }
+    >
+      <div className={statsStyles.cardContainer}>
+        {languageStats()}
+        {AllProblemsSolved()}
+      </div>
+      <div className={statsStyles.cardContainer}>{topicStats()}</div>
+    </SolutionLayout>
+  );
+}
