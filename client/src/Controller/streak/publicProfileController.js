c88d745fa434d72e512ba5db2c21c08454e6e985
FengDenny <fengdennyy@gmail.com>
2023-02-09 15:26:15 -0800

    Removed and  replaced by POSTQueries template

--- client/src/Controller/streak/publicProfileController.js
+++
@@ -1,7 +0,0 @@
-import axios from "axios";
-
-export const publicProfileController = async (data) =>
-  await axios.post(
-    "https://warframeleeter.onrender.com/userProfileCalendar",
-    data
-  );
