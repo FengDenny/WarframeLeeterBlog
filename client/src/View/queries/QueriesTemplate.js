c6bf96c87de972726eb5c549aed09f20d696cabc
FengDenny <fengdennyy@gmail.com>
2023-02-09 15:17:24 -0800

    Added GETQueries/ changed Queries to POSTQueries

--- client/src/View/queries/QueriesTemplate.js
+++ client/src/View/queries/QueriesTemplate.js
@@ -1,11 +1,21 @@
 import { useState, useEffect } from "react";
-import { postEndpoint } from "../../Controller/endpoint";
-export const Queries = (endpoint, user) => {
+import { postEndpoint, getEndpoint } from "../../Controller/endpoint";
+export const POSTQueries = (endpoint, user) => {
   const [username, _] = useState(user);
   const [data, setData] = useState("");
   useEffect(() => {
     const data = { username };
     postEndpoint(endpoint, data).then((items) => setData(items.data));
+  }, [username, endpoint]);
+  return { data };
+};
+
+export const GETQueries = (endpoint, user) => {
+  const [username, _] = useState(user);
+  const [data, setData] = useState("");
+  useEffect(() => {
+    const data = { username };
+    getEndpoint(endpoint, data).then((items) => setData(items.data));
   }, [username, endpoint]);
   return { data };
 };
