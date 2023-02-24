3ab2baca9d018a0edbccba17821406606506fc17
FengDenny <fengdennyy@gmail.com>
2023-02-23 19:19:54 -0800

    Implemented years array to query by years for POST request

---
+++ client/src/View/queries/profile/userProfile.js
@@ -0,0 +1,9 @@
+import { POSTQueriesByYear } from "../QueriesTemplate";
+
+export const yearsArray = [2021, 2022, 2023];
+
+export const yearEndpoints = () => {
+  return yearsArray.map((year) =>
+    POSTQueriesByYear("userProfileCalendarByYear", "warframeleeter", year)
+  );
+};
