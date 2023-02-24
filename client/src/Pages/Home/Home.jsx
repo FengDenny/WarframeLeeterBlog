6af4ad4aa9a5dc8f6f57ca4cebd1a877ceefbb76
FengDenny <fengdennyy@gmail.com>
2023-02-24 14:23:46 -0800

    Added Profile section

--- client/src/Pages/Home/Home.jsx
+++ client/src/Pages/Home/Home.jsx
@@ -3,6 +3,7 @@ import Hero from "./Hero";
 import Solution from "./Solution";
 import DailysSolved from "./DailysSolved";
 import Statistics from "./Statistics";
+import Profile from "./Profile";
 
 export default function Home() {
   return (
@@ -11,6 +12,7 @@ export default function Home() {
       <Solution />
       <DailysSolved />
       <Statistics />
+      <Profile />
     </div>
   );
 }
