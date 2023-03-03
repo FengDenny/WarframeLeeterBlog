5697f269b4e1e4c31e93bbd0c568a849cb5c7156
FengDenny <fengdennyy@gmail.com>
2023-03-03 00:23:16 -0800

    Created spinner span for styling

--- client/src/components/suspense/Spinner.jsx
+++ client/src/components/suspense/Spinner.jsx
@@ -2,4 +2,8 @@ import { FaSpinner } from "react-icons/fa";
 
 import loaderStyles from "../../components/suspense/loader.module.scss";
 
-export const Spinner = () => <FaSpinner className={loaderStyles.loadingIcon} />;
+export const Spinner = ({ style }) => (
+  <span className={loaderStyles.loadingIcon}>
+    <FaSpinner className={style} />
+  </span>
+);
