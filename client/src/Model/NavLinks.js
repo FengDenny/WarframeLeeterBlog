b8e0ba14f53a5def5e478bb051495f908ba920d3
FengDenny <fengdennyy@gmail.com>
2023-03-07 22:55:02 -0800

    Added more  links  to show social media accounts

---
+++ client/src/Model/NavLinks.js
@@ -0,0 +1,33 @@
+export const links = [
+  {
+    id: 1,
+    name: "Home",
+    ahref: "/",
+  },
+  {
+    id: 2,
+    name: "Achievements",
+    ahref: "/achievements",
+  },
+  {
+    id: 3,
+    name: "Solutions",
+    ahref: "/solutions",
+  },
+
+  {
+    id: 4,
+    name: "GitHub",
+    ahref: "https://github.com/FengDenny",
+  },
+  {
+    id: 5,
+    name: "LeetCode",
+    ahref: "https://leetcode.com/WarframeLeeter/",
+  },
+  {
+    id: 6,
+    name: "Linkedin",
+    ahref: "https://www.linkedin.com/in/denny-feng/",
+  },
+];
