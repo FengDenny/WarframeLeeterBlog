db70b7e6762b543699e923d7b497f801bfc34cba
FengDenny <fengdennyy@gmail.com>
2023-03-07 22:48:51 -0800

    Added  Footer, Disclaimer banner

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,20 @@
 import { Main } from "./MainRoutes";
 import Navbar from "./components/util/Navbar/Navbar.jsx";
+import Footer from "./components/util/Footer/Footer.jsx";
+
+import Disclaimer from "./components/util/Banner/Disclaimer";
 
 function App() {
   return (
     <div>
+      <Disclaimer />
       <Navbar />
-      <main>
+
+      <main id='main'>
         <Main />
       </main>
+
+      <Footer copyright={"© 2023 WarfameLeeter.com. All rights reserved."} />
     </div>
   );
 }
