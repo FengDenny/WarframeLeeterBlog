fec8deb13f42d723248a93729ebc8af1c39fb384
FengDenny <fengdennyy@gmail.com>
2023-03-07 22:49:12 -0800

    removed old animations component

--- client/src/components/animations/AnimateOnScroll.jsx
+++ client/src/components/animations/AnimateOnScroll.jsx
@@ -1,30 +1,4 @@
-import { useEffect, useRef, useLayoutEffect, useState } from "react";
-
-const isElementInViewport = (items) => {
-  let rect = items.getBoundingClientRect();
-  return (
-    rect.top >= 0 &&
-    rect.left >= 0 &&
-    rect.bottom <=
-      (window.innerHeight || document.documentElement.clientHeight) &&
-    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-  );
-};
-
-export const AnimateOnScroll = (items, style) => {
-  return useEffect(() => {
-    function callbackFunc() {
-      for (let i = 0; i < items?.length; i++) {
-        if (isElementInViewport(items[i])) {
-          items[i].classList.add(style);
-        } else {
-          items[i].classList.remove(style);
-        }
-      }
-    }
-    window.addEventListener("scroll", callbackFunc);
-  }, [items, style]);
-};
+import { useRef, useLayoutEffect, useState } from "react";
 
 export const AnimateOnScrollObserver = ({ style, children }) => {
   const [visible, setVisible] = useState();
@@ -52,5 +26,37 @@ export const AnimateOnScrollObserver = ({ style, children }) => {
     <li ref={ref} className={visible ? style : null}>
       {children}
     </li>
+  );
+};
+export const AnimateSpanOnScrollObserver = ({
+  style,
+  children,
+  notVisible,
+}) => {
+  const [visible, setVisible] = useState();
+  const ref = useRef();
+
+  const thresholdArray = () => {
+    const threshold = [];
+    for (let i = 0; i <= 1; i += 0.01) threshold.push(i);
+    return threshold;
+  };
+
+  useLayoutEffect(() => {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          setVisible(entry.isIntersecting);
+        });
+      },
+      { threshold: thresholdArray() }
+    );
+    observer.observe(ref.current);
+  }, [setVisible]);
+
+  return (
+    <span ref={ref} className={visible ? style : notVisible}>
+      {children}
+    </span>
   );
 };
