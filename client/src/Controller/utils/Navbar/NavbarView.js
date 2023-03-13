87cef30ff3a103a38732af63175318cf76ee1862
FengDenny <fengdennyy@gmail.com>
2023-03-13 15:23:59 -0700

    Removed useEffect as it is not used

--- client/src/Controller/utils/Navbar/NavbarView.js
+++ client/src/Controller/utils/Navbar/NavbarView.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import { useState } from "react";
 
 export const MobileMenuToggler = () => {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
