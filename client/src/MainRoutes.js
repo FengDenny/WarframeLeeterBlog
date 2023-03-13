4ccfb47bbf1173685a4c2ec37a069890cbc9b354
FengDenny <fengdennyy@gmail.com>
2023-03-13 15:23:13 -0700

    Added path for Solutions page

--- client/src/MainRoutes.js
+++ client/src/MainRoutes.js
@@ -1,10 +1,15 @@
 import { useRoutes } from "react-router-dom";
 import Home from "./View/Home/Home.jsx";
 import Achievements from "./View/Achievements/Achievements.jsx";
+import Solutions from "./View/Solutions/Solutions.jsx";
 export const Main = () => {
   let routes = useRoutes([
     { path: "/", element: <Home /> },
     { path: "/achievements", element: <Achievements /> },
+    {
+      path: "/solutions",
+      element: <Solutions />,
+    },
   ]);
 
   return routes;
