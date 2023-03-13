e93c63c3976f7ce05ad9ad5b4174ab224d5576ac
FengDenny <fengdennyy@gmail.com>
2023-03-13 15:20:57 -0700

    Moved Searchbar to own folder in utils

--- client/src/components/util/Searchbar/Searchbar.jsx
+++ client/src/components/util/Searchbar/Searchbar.jsx
@@ -1,11 +1,25 @@
 import React from "react";
-import navbarStyles from "../../../../scss/util/Navbar/navbar.module.scss";
-import mobileStyles from "../../../../scss/util/Navbar/mobileNavbar.module.scss";
+import navbarStyles from "../../../scss/util/Navbar/navbar.module.scss";
+import mobileStyles from "../../../scss/util/Navbar/mobileNavbar.module.scss";
 import { FaSearch } from "react-icons/fa";
-export default function Searchbar({ mobile }) {
+
+import { useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { searchAction } from "../../../redux/slice/SearchTitleSlice.js";
+
+export default function Searchbar({
+  mobile,
+  style,
+  btnStyle,
+  searchTitle,
+  setSearchTitle,
+}) {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
   return (
     <form
-      className={`${
+      className={`${style} ${
         mobile
           ? mobileStyles.searchbarContainer
           : navbarStyles.searchbarContainer
@@ -21,16 +35,24 @@ export default function Searchbar({ mobile }) {
         className={`${
           mobile ? mobileStyles.searchbar : navbarStyles.searchbar
         }`}
+        onChange={(e) => {
+          setSearchTitle(e.target.value);
+          dispatch(searchAction.searchTitle(e.target.value));
+        }}
       />
       <div
         className={`${
           mobile ? mobileStyles.btnContainer : navbarStyles.btnContainer
-        }`}
+        } ${btnStyle}`}
       >
         <button
           className={`${
             mobile ? mobileStyles.searchBtn : navbarStyles.searchBtn
           }`}
+          onClick={() => {
+            navigate("/solutions");
+            dispatch(searchAction.searchTitle(searchTitle));
+          }}
         >
           <FaSearch
             className={`${
